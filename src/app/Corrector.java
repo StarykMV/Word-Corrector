951607458f4646406193f5f574a0d739aab857c2
Miroslav <myroslav.staryk@viti.edu.ua>
2025-04-24 12:36:34 +0300

    Fix Corrector and DataProvider classes for word correction functionality

--- src/app/Corrector.java
+++ src/app/Corrector.java
@@ -1,4 +1,19 @@
 package app;
 
 public class Corrector {
+    public String handleData(String[] strs){
+        StringBuilder stringBuilder = new StringBuilder();
+        int count = 1;
+
+        for (String str : strs) {
+            if(str.contains("b")) {
+                str = str.replace("b", "o");
+            }
+
+            stringBuilder.append(str).append(") ").append(str).append("\n");
+            count++;
+        }
+
+        return stringBuilder.toString();
+    }
 }
