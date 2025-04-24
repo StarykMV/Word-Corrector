0592ca14ce72f87493f0b15dac5064d71a3b56d3
Miroslav <myroslav.staryk@viti.edu.ua>
2025-04-24 12:56:50 +0300

    Fix word correction logic in Corrector.java

--- src/app/Corrector.java
+++ src/app/Corrector.java
@@ -1,19 +1,19 @@
 package app;
 
 public class Corrector {
-    public String handleData(String[] strs){
+
+    public String handleData(String[] strs) {
         StringBuilder stringBuilder = new StringBuilder();
         int count = 1;
 
         for (String str : strs) {
-            if(str.contains("b")) {
+            if (str.contains("b")) {
                 str = str.replace("b", "o");
             }
 
-            stringBuilder.append(str).append(") ").append(str).append("\n");
+            stringBuilder.append(count).append(") ").append(str).append("\n");
             count++;
         }
-
         return stringBuilder.toString();
     }
 }
