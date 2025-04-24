b3330692b23fd68fdcc2218759d2cdaa30584f43
Miroslav <myroslav.staryk@viti.edu.ua>
2025-04-24 12:49:11 +0300

    Add user input functionality to Main.java

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,34 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        DataProvider dataProvider = new DataProvider();
+        Corrector corrector = new Corrector();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of words:");
+        int count = scanner.nextInt();
+        scanner.nextLine();
+
+        String[] userWords = new String[count];
+        System.out.println("Enter " + count + " words:");
+
+        for (int i = 0; i < count; i++) {
+            userWords[i] = scanner.nextLine();
+        }
+
+        String result = corrector.handleData(userWords);
+
+        getOutput(result);
+
+        scanner.close();
+    }
+
+    private static void getOutput(String output) {
+        System.out.println("Corrected words:");
+        System.out.println(output);
+    }
 }
